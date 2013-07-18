8a2e6e813a3c699af932ef15b48949bbe5974326
Brian Chu <bc@brianchu.com>
2013-07-18 16:57:58 -0700

    Grunt exclude watch files

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,7 +44,7 @@ module.exports = function (grunt) {
     },
     watch: {
       dev: {
-        files: ['**/*{.js,.html,.yml,.md,.markdown,.css,.sass,.scss}', '!public/**'],
+        files: ['**/*{.js,.html,.yml,.md,.markdown,.css,.sass,.scss}', '!public/**', '!_deploy/**', '!node_modules/**'],
         tasks:['exec:preview'],
         options: {
           livereload:LIVERELOAD_PORT
