64cab535f2896fb34fb4d88d3b48472d89631723
Brian Chu <bc@brianchu.com>
2013-07-30 17:24:48 -0700

    Updated setup deploy script with gitconfig author info

--- setup_github_pages.sh
+++ setup_github_pages.sh
@@ -1,6 +1,8 @@
 mkdir -p _deploy
 cd _deploy
 git init
+git config user.name "Brian Chu"
+git config user.email bc@brianchu.com
 git remote add origin https://bchu@github.com/bchu/bchu.github.io.git
 git pull -u origin master
 cd ..
